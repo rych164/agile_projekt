c945ac7dada366359f210868952644da90dd9931
rych164 <107195029+rych164@users.noreply.github.com>
2023-10-30 13:53:25 +0100

    zakodowana klasa użytkownicy

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, url_for, request, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required
 
@@ -21,8 +22,16 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-class Users():
-    pass
+class Users(db.Model):
+    user_id = db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(200),nullable=False)
+    surname=db.Column(db.String(200),nullable=False)
+    email=db.Column(db.String(200),nullable=False)
+    password=db.Column(db.String(200),nullable=False)
+    date_time_of_account_creation=db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route("/")
 def index():
