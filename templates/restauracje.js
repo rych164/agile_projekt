678a91053bc54877bdcc2af597a53bb1cf42e282
Patryk <patryk.pietruszewski@edu.uekat.pl>
2024-01-07 00:03:04 +0100

    Z jakiegoś powodu nie działa

--- templates/restauracje.js
+++ templates/restauracje.js
@@ -1,3 +1,20 @@
-function pokaz() {
-    document.getElementByClassName("wynik").innerHTML="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    }
+function p5() {
+	document.getElementById("wynik").innerHTML="Hotel Wisła Premium";
+    document.getElementById("tresc").innerHTML="Hotele Wisła SPA to 3* hotel gwarantujący najlepsze ceny w Wiśle. W cenie <br>pobytu oferujemy: śniadanie bufetowe, parking, sauny, grota solna, billard <br>oraz wodę mineralną i zestaw do kawy i herbaty w pokoju. Dopełnieniem <br>oferty jest Nagradzana Restauracja.<br><br> <button onclick='guzik1()'>Sprawdź naszą stronę!</button><br><br> <button onclick='formularz1()'>Oblicz cenę pobytu</button>";
+	}
+function p1() {
+    document.getElementById("wynik").innerHTML="Hotel Gołębiewski Wisła";
+    document.getElementById("tresc").innerHTML="Hotel usytuowany jest na zboczu <br>Bukowej nad brzegiem Wisły. <br>Z okien Hotelu roztacza się niepowtarzalny <br>widok na urokliwe zakątki Beskidu Śląskiego.<br><br> <button onclick='guzik2()'>Sprawdź naszą stronę!</button><br><br> <button onclick='formularz2()'>Oblicz cenę pobytu</button>";
+}
+function p2() {
+    document.getElementById("wynik").innerHTML="Hotel Arka Spa";
+    document.getElementById("tresc").innerHTML="Hotel „Arka SPA” jest jedna z najnowszych baz noclegowych w Wisle. <br>Posiada 50 miejsc noclegowych w pokojach dwu- i cztero- osobowych <br>oraz apartamentach z pelnym wezlem sanitarnym <br>plus 32 miejsca w domkach typu Brda. <br>Do dyspozycji gosci udostepniamy restauracje, bar, sale konferencyjna <br>zas z atrakcji rekreacyjnych oferujemy centrum <br>Spa oraz kryty basen. Monitorowany parking gratis!!!<br>Wnetrze hotelu zostalo wykonane z wielka dbaloscia. Urzedowo obiekt zaklasyfikowany zostal <br>do standardu dwóch gwiazdek, ale dobrane ze smakiem elementy oryginalnego <br>wystroju oraz wyposazenie pozwalaja naszym Gosciom na uczucie pelnego luksusu hotelu <br>o standardzie czterogwiazdkowym. <br>Hotel nie przyjmuje zwierząt.<br><br> <button onclick='guzik5()'>Sprawdź naszą stronę!</button><br><br> <button onclick='formularz3()'>Oblicz cenę pobytu</button>";
+}
+function p3() {
+    document.getElementById("wynik").innerHTML="Hotel Pod Gołębiem";
+    document.getElementById("tresc").innerHTML="Nasz obiekt dysponuje dwudziestoma dwoma eleganckimi i <br>przytulnymi pokojami w których jednorazowo możemy ugościć pięćdziesiąt dwie osoby. <br>Do dyspozycji Gości w sezonie letnim oddajemy basen z ogrzewaną wodą i <br>trampolinę dla dzieci. Na terenie hotelu na naszych <br>najmłodszych gości czeka również sala zabaw, <br>poza tym sala bilardowa, mini strefa Wellness z <br>masażem Hydro - Jet oraz fińską sauną. W całym hotelu oferujemy <br>bezpłatny dostęp do Internetu poprzez wewnętrzną sieć Wi-Fi, <br>jak również bezpłatny parking i przechowalnię nart i rowerów.<br><br> <button onclick='guzik3()'>Sprawdź naszą stronę!</button><br><br> <button onclick='formularz4()'>Oblicz cenę pobytu</button>";
+}
+function p4() {
+    document.getElementById("wynik").innerHTML="Hotel Pod Jedlami";
+    document.getElementById("tresc").innerHTML="Jeśli lubisz atmosferę naszej miejscowości – Hotel Pod Jedlami*** <br>położony jest 15 minut spacerem od wiślańskiego deptaka z całym jego dobrodziejstwem. <br>Skorzystaj z tego! <br>Jeśli chcesz odpocząć – hotel jest położony na skraju lasu, na zboczu góry Jarzębatej w Wiśle. <br>Goście bardzo chwalą ciszę, która pozwala odpocząć. <br>Wiosenne i letnie poranki często okraszone są – po prostu – śpiewem ptaków. <br>Odpocznij! Jeśli lubisz aktywną turystykę - bezpośrednio przy <br>hotelu znajdziesz szlak turystyczny na Trzy Kopce Wiślańskie. <br>Nasi Goście często wykorzystują obiekt jako bazę <br>wypadową na wędrówki górskie, ale także wycieczki do Trójwsi, <br>Ustronia czy Cieszyna. Przy wejściu do hotelu znajdziesz kijki <br>do Nordic Walking, które ułatwią i umilą spacery.<br><br> <button onclick='guzik5()'>Sprawdź naszą stronę!</button><br><br> <button onclick='formularz4()'>Oblicz cenę pobytu</button>";
+}
